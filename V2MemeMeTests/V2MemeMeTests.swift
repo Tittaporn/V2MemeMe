22f4b7e89ebe9a7eadf2f8ce1bfc89e1cd15e7cf
Lee <tittaporn_saelee@icloud.com>
2020-10-05 01:11:06 -0500

    Initial Commit

---
+++ V2MemeMeTests/V2MemeMeTests.swift
@@ -0,0 +1,33 @@
+//
+//  V2MemeMeTests.swift
+//  V2MemeMeTests
+//
+//  Created by Lee McCormick on 10/5/20.
+//
+
+import XCTest
+@testable import V2MemeMe
+
+class V2MemeMeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
